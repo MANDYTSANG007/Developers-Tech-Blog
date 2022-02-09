bcf916a9fdd20fcc32683fbec5f6304b2705a5f0
Mandy Tsang <mandy.tsang007@gmail.com>
2022-02-08 22:56:06 -0800

    add relationships to index.js model

--- models/Post.js
+++ models/Post.js
@@ -37,4 +37,3 @@ Post.init(
 );
 
 module.exports = Post;
-
--- models/index.js
+++ models/index.js
@@ -4,17 +4,30 @@ const Comment = require("./Comment");
 
 
 //user can make many posts
-
+User.hasMany(Post, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+});
 // user can make many comments
-
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+})
 // post only belongs to one user
-
+Post.belongsTo(User, {
+    foreignKey: "user_id"
+})
 // post can have many comments
-
+Post.hasMany(Comment, {
+    foreignKey: "post_id"
+})
 //comment only belongs to one user
-
+Comment.belongsTo(User, {
+    foreignKey: "user_id"
+})
 //comment only belongs to one post
-
+Comment.belongsTo(Post, {
+    foreignKey: "post_id"
+})
 
 
 module.exports = { User, Post, Comment};
