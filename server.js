a93034c8d42e7ceffae68432c971d002201ba94f
Mandy Tsang <mandy.tsang007@gmail.com>
2022-02-08 20:57:39 -0800

    import dependencies to server.js

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 // Import dependencies
 const express = require("express");
+const sequelize = require("./config/connection");
+const path = require("path");
+
+// Import handlebars
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({helpers});
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set handlebars as the default template engine
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// Turn on routes
+app.use(routes);
+
+// Turn on connection to the database
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log("Now listening"));
+});
