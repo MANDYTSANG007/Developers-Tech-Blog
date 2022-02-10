506f8a7d11f6276c01a34e49c6ae14cedf642106
Mandy Tsang <mandy.tsang007@gmail.com>
2022-02-09 23:46:22 -0800

    add 'GET all comment', 'CREATE a comment', and 'UPDATE a comment' routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,58 @@
+const router = require("express").Router();
+const { route } = require(".");
+const { User, Post, Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+// GET all comments
+router.comment("/", withAuth, async (req, res) => {
+    try{
+        const commentData = await Comment.findAll({
+            include: [
+                { 
+                    model: User,
+                },
+                {
+                    model: Post,
+                    attibutes: ["id", "title", "content"]
+                } 
+            ],
+        });
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// CREATE a comment
+route.post("/", async(req, res) => {
+    try{
+        const commentData = await Comment.create({
+            comment_text: req.body.comment_text,
+            user_id: req.session.user_id,
+            post_id: req.body.post_id,
+        });
+        res.status(200).json(commentData);
+    } catch (err){
+        res.status(400).json(err);
+    }
+});
+// UPDATE a comment
+route.put("/:id", async(req, res) => {
+    Comment.update(req.body, {
+        where: {
+            id: req.params.id
+        },
+    }).then(commentData => {
+        if(!commentData){
+            res.status(404).json({ message: "No comment found with this id"});
+            return;
+        }
+        res.status(200).json(commentData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+// DELETE a comment
+
