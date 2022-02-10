6d5fa06271950c2dc8552ef81ae219adb5b7cdfa
Mandy Tsang <mandy.tsang007@gmail.com>
2022-02-09 21:25:15 -0800

    add 'delete a user' route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { route } = require(".");
 const { User, Post } = require("../../models");
 
 // GET users (find all users)
@@ -38,9 +39,23 @@ router.get("/:id", async(req, res) => {
     });
 });
 
-
-
-
+// UPDATE a user
+router.put()("/:d", async(req, res) => {
+    User.update(req.body, {
+        where: {
+            id: req.params.id
+        },
+    }).then(categoryData => {
+        if(!userData) {
+            res.status(404).json({message: "No category found with this id!"});
+            return;
+        }
+        res.status(200).json(userData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
 
 //CREATE a new user
 router.post("/", async (req, res) => {
@@ -54,6 +69,24 @@ router.post("/", async (req, res) => {
         });
     } catch (err) {
         res.status(400).json(err);
+    }
+});
+
+// DELETE a user
+router.delete("/:id", async(req, res) => {
+    try{
+        const userData = await User.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        if (!userData) {
+            res.status(404).json({ message: "No user found with this id!"});
+            return;
+        }
+        res.status(200).json(userData);
+    } catch(err) {
+        res.status(500).json(err);
     }
 });
 
