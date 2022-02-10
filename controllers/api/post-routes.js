fb6bc5cdcff2277f2e63f0911ab8e36989194105
Mandy Tsang <mandy.tsang007@gmail.com>
2022-02-09 22:06:49 -0800

    add 'DELETE a post' route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -74,7 +74,25 @@ router.put("/:id", async(req, res) => {
     });
 });
 
+// DELETE a post
+router.delete("/:id", async(req, res) =>{
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        if (!postData) {
+            res.status(404).json ({ message: "No post found with this id!"});
+            return;
+        }
+        res.status(200).json(postData);
+    } catch (err){
+        res.status(500).json(err);
+    }
+});
 
+module.exports = router;
 
 
 
