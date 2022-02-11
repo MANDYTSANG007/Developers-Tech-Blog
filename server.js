8f77a588cbb7bf467e7d4c63a2c442ed5849061f
Mandy Tsang <mandy.tsang007@gmail.com>
2022-02-10 16:40:48 -0800

    set up express-session

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
 // Import dependencies
 const express = require("express");
-const sequelize = require("./config/connection");
+const session = require("express-session");
 const path = require("path");
+const routes = require("./controllers");
+const helpers = require("./utils.helpers");
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 // Import handlebars
 // Set up handlebars engine with custom helpers
 const exphbs = require("express-handlebars");
+const exp = require("constants");
 const hbs = exphbs.create({helpers});
 
-// Sets up the Express App
+// Set up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Create session
+const sess = {
+    secret: "Super secret secret",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Set handlebars as the default template engine
 app.engine("handlebars", hbs.engine);
